8cf384e2bef74e071bcf156a1b2564b81a22e423
stell <sokolov.a.a83@gmail.com>
2022-03-20 21:20:29 +0300

    deploy fix1

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/api', apiRouter)
 
